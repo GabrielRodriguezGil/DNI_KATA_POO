7e89e24e28333ac81d34dcf94cab6e8e9469aa49
goskus23 <141594539+goskus23@users.noreply.github.com>
2026-02-08 22:32:09 +0100

    refactor(test): :recycle: Refactorizar los test para los nombres de funciones usadas.

--- test/test_dni_cif.py
+++ test/test_dni_cif.py
@@ -9,71 +9,69 @@ def inyector():
     return Dni()
 
 def test_constructor_default(dni):
-    assert dni.getDni() == ""
-    assert not dni.getNumeroSano()
-    assert not dni.getLetraSana()
+    assert dni.get_dni() == ""
+    assert not dni.get_numero_sano()
+    assert not dni.get_letra_sana()
 
 def test_setters_getters(dni):
-    dni.setDni("12345678Z")
-    assert dni.getDni() == "12345678Z"
-
+    dni.set_dni("12345678Z")
+    assert dni.get_dni() == "12345678Z"
 @pytest.mark.parametrize("dni_test", CASOS_TEST_CORRECTOS)
 def test_check_cif_correcto(dni, dni_test):
-    dni.setDni(dni_test)
-    assert dni.checkCIF()
+    dni.set_dni(dni_test)
+    assert dni.check_CIF()
 
 @pytest.mark.parametrize("dni_test", CASOS_TEST_LETRA_PROHIBIDA)
 def test_check_cif_incorrecto(dni, dni_test):
-    dni.setDni(dni_test)
-    assert not dni.checkCIF()
+    dni.set_dni(dni_test)
+    assert not dni.check_CIF()
 
 @pytest.mark.parametrize("dni_test", CASOS_TEST_CORRECTOS)
 def test_check_dni_correcto(dni, dni_test):
-    dni.setDni(dni_test)
-    assert dni.checkDni()
+    dni.set_dni(dni_test)
+    assert dni.check_DNI()
 
 @pytest.mark.parametrize("dni_test", CASOS_TEST_FORMATO_INCORRECTO)
 def test_check_dni_incorrecto(dni, dni_test):
-    dni.setDni(dni_test)
-    assert not dni.checkDni()
+    dni.set_dni(dni_test)
+    assert not dni.check_DNI()
 
 @pytest.mark.parametrize("dni_test", CASOS_TEST_CORRECTOS)
 def test_check_letra_correcta(dni, dni_test):
-    dni.setDni(dni_test)
-    dni.checkDni()  # Necesario para establecer numeroSano
-    assert dni.checkLetra()
+    dni.set_dni(dni_test)
+    dni.check_DNI()  # Necesario para establecer numeroSano
+    assert dni.check_valid_letter()
 
 @pytest.mark.parametrize("dni_test", CASOS_TEST_LETRA_PROHIBIDA)
 def test_check_letra_incorrecta(dni, dni_test):
-    dni.setDni(dni_test)
-    dni.checkDni()  # Necesario para establecer numeroSano
-    assert not dni.checkLetra()
+    dni.set_dni(dni_test)
+    dni.check_DNI()  # Necesario para establecer numeroSano
+    assert not dni.check_valid_letter()
 
 @pytest.mark.parametrize("dni_test", CASOS_TEST_FORMATO_INCORRECTO)
 def test_check_letra_numero_mal_formateado(dni, dni_test):
-    dni.setDni(dni_test)
-    dni.checkDni()  # Necesario para establecer numeroSano
-    assert not dni.checkLetra()
+    dni.set_dni(dni_test)
+    dni.check_DNI()  # Necesario para establecer numeroSano
+    assert not dni.check_valid_letter()
 
 @pytest.mark.parametrize("dni_test", CASOS_TEST_CORRECTOS)
 def test_obtener_letra_correcta(dni, dni_test):
-    dni.setDni(dni_test)
-    dni.checkDni()  # Necesario para establecer numeroSano
-    assert dni.obtenerLetra() == dni_test[-1]
+    dni.set_dni(dni_test)
+    dni.check_DNI()  # Necesario para establecer numeroSano
+    assert dni.get_alphabetic_part() == dni_test[-1]
 
 def test_obtener_letra_sin_numero_sano(dni):
-    dni.setDni("1234567X")  # DNI con longitud incorrecta
-    assert dni.obtenerLetra() is None
-
+    dni.set_dni("1234567X")  # DNI con longitud incorrecta
+    assert dni.calculate_letter() is None
 def test_parte_alfabetica_dni(dni):
-    dni.setDni("12345678Z")
-    assert dni.getParteAlfabeticaDni() == "Z"
+    dni.set_dni("12345678Z")
+    assert dni.get_alphabetic_part() == "Z"
 
 def test_parte_numerica_dni(dni):
-    dni.setDni("12345678Z")
-    dni.checkDni()  # Necesario para establecer numeroSano
-    assert dni.getParteNumericaDni() == "12345678"
+    dni.set_dni("12345678Z")
+    dni.check_DNI()  # Necesario para establecer numeroSano
+    assert dni.get_numeric_part() == "12345678"
 
 def test_parte_numerica_dni_sin_numero_sano(dni):
-    dni.setDni("1234567")  # DNI incompleto
-    assert not dni.getParteNumericaDni()
+    dni.set_dni("1234567")  # DNI incompleto
+    assert not dni.get_numeric_part()
